9b3522c4e2e489343df2ff14f0ecb09c28448073
saltyJeff <saltyJeff@users.noreply.github.com>
2018-11-06 22:33:20 -0800

    readability enhancement

--- base/parse_struct.py
+++ base/parse_struct.py
@@ -4,27 +4,28 @@ from gen import STRUCT_ORDER, STRUCT_LENGTH
 
 parse_buffer: bytearray = bytearray(STRUCT_LENGTH)
 contig_ff = 0 #counts the number of contiguous FF's
-write_index = STRUCT_LENGTH+1 #counts which index to write the next byte into
-
+write_index = STRUCT_LENGTH #counts which index to write the next byte into
+throwaway = True #throwaway the first data (is junk)
 def parse_struct(bytes: bytearray) -> List[tuple]:
-    global write_index, contig_ff
+    global write_index, contig_ff, throwaway
     tuple_list = []
     for b in bytes:
+        if write_index < STRUCT_LENGTH:
+            parse_buffer[write_index] = b
+            write_index = write_index + 1
         if b == 255:
             contig_ff = contig_ff + 1
         else:
             contig_ff = 0
         if contig_ff == 8:
             if write_index == STRUCT_LENGTH:
-                tuple_list.append(
-                    unpack(STRUCT_ORDER, parse_buffer)
-                )
-            write_index = 0
-            contig_ff = 0
-            continue # don't write the FF into the 0 slot
-        if write_index < STRUCT_LENGTH:
-            parse_buffer[write_index] = b
-            write_index = write_index + 1
+                if not throwaway:
+                    tuple_list.append(
+                        unpack(STRUCT_ORDER, parse_buffer)
+                    )
+                throwaway = False
+                contig_ff = 0 
+                write_index = 0
     return tuple_list
 
         
